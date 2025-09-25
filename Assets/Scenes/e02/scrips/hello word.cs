620c19ebba05d7490fd42db115ad893185717693
adrià coral - AJEI <adria.coral@estudiant.enti.cat>
2025-09-25 11:13:49 +0200

    mini ej.

---
+++ Assets/Scenes/e02/scrips/hello word.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class helloword : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("hello word");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
