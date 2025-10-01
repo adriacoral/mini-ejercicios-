563dc2fb1540d92327cc28b14a4a01c31877f3d4
adrià coral - AJEI <adria.coral@estudiant.enti.cat>
2025-10-01 13:14:29 +0200

    he hecho el ejercicio 4 y 5

---
+++ Assets/Scenes/e05/scripst/decimalesP.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class decimalesP : MonoBehaviour
+{
+    public float decimales = 0.05f;
+    public float decimales2 = 0.09f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print(decimales + decimales2);
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
